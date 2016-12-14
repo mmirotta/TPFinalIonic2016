f7f7438124bdfae78b9626288e4d20ab35ad9e11
matias mirotta <m.mirotta@gmail.com>
2016-12-14 12:21:49 -0300

    Filtro para mis batallas

--- BatallaNaval/www/js/batallas.js
+++ BatallaNaval/www/js/batallas.js
@@ -54,25 +54,81 @@ angular.module('starter.batallas', [])
       {
         $state.go("login");
     }
-
+    $scope.buscar = {};
+    $scope.buscar.filtro = "Activas";
+    $scope.buscando = false;
     $scope.ListaBatallas = [];
 
     Servicio.Cargar('/batalla').on('child_added',function(snapshot)
     { 
       $timeout(function(){
         var batalla = snapshot.val();
-        if ((batalla.usuarioCreador.nombre == $scope.usuario.nombre 
-          || batalla.usuarioAcepta.nombre == $scope.usuario.nombre) 
-          && (batalla.aceptada == true))
-          $scope.ListaBatallas.push(batalla);
+        if (batalla.usuarioCreador.nombre == $scope.usuario.nombre || batalla.usuarioAcepta.nombre == $scope.usuario.nombre)
+        {
+          switch ($scope.buscar.filtro)
+          {
+            case "Activas":
+                if (batalla.aceptada == true && batalla.finalizada == false)
+                  $scope.ListaBatallas.push(batalla);
+              break;
+
+            case "Finalizadas":
+                if (batalla.finalizada == true)
+                  $scope.ListaBatallas.push(batalla);
+              break;
+
+            case "Tu turno":
+                if (batalla.aceptada == true && batalla.finalizada == false && batalla.turno.nombre == $scope.usuario.nombre)
+                  $scope.ListaBatallas.push(batalla);
+              break;
+          }
+        }
       });
     });
-
   }
   catch(error)
   {
     console.info("Ha ocurrido un error en MisBatallasCtrl. " + error);
   }
+
+  $scope.Busqueda = function(){
+    try
+    {
+      $scope.buscando = true;
+      $scope.ListaBatallas = [];
+      Servicio.Cargar('/batalla').on('child_added',function(snapshot)
+      { 
+        $timeout(function(){
+          var batalla = snapshot.val();
+          if (batalla.usuarioCreador.nombre == $scope.usuario.nombre || batalla.usuarioAcepta.nombre == $scope.usuario.nombre)
+          {
+            switch ($scope.buscar.filtro)
+            {
+              case "Activas":
+                  if (batalla.aceptada == true && batalla.finalizada == false)
+                    $scope.ListaBatallas.push(batalla);
+                break;
+
+              case "Finalizadas":
+                  if (batalla.finalizada == true)
+                    $scope.ListaBatallas.push(batalla);
+                break;
+
+              case "Tu turno":
+                  if (batalla.aceptada == true && batalla.finalizada == false && batalla.turno.nombre == $scope.usuario.nombre)
+                    $scope.ListaBatallas.push(batalla);
+                break;
+            }
+          }
+        });
+      });
+      $scope.buscando = false;
+    }
+    catch (error)
+    {
+      console.info("Ha ocurrido un error en MisBatallasCtrl-Buscar. " + error);
+    }
+  };
 
   $scope.JugarBatalla = function(batalla){
     var param = JSON.stringify(batalla);
